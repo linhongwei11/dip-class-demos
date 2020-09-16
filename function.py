8660b56878fd0b7101f3114c274f4ae8e9383d8e
lin ge <397367585@qq.com>
2020-09-16 10:55:54 +0800

    增加备注信息

--- function.py
+++ function.py
@@ -10,10 +10,46 @@ def filtermat():
     ret, frame = cap.read()
     if ret:
         # 中值滤波
+        """
+        medianBlur（） 函数模型：
+        medianBlur( InputArray src, OutputArray dst, int ksize );
+    
+        参数介绍：
+        . InputArray src：InputArray类型的src，可以是多通道的Mat类型的图像；但深度应CV_8U，CV_16U，CV_16S，CV_32F
+        . OutputArray dst：OutputArray类型的dst，即目标图像，函数的输出参数，需要和源图片有一样的尺寸和类型。
+        . int ksize：int类型的ksize，孔径的线性尺寸（aperture linear size），注意这个参数必须是大于1的奇数
+        """
         median_mat = cv.medianBlur(frame, 3)
         # 均值滤波
+        """
+        blur（） 函数模型：
+        blur( InputArray src, OutputArray dst,
+                            Size ksize, Point anchor = Point(-1,-1),
+                            int borderType = BORDER_DEFAULT );
+    
+    
+        参数介绍：
+        . InputArray src:InputArray类型的src，可以是多通道的Mat类型的图像；但深度应CV_8U，CV_16U，CV_16S，CV_32F
+        . OutputArray dst:OutputArray类型的dst，即目标图像，函数的输出参数，需要和源图片有一样的尺寸和类型。
+        . Size ksize:Size类型的ksize，内核的大小
+        . Point anchor:Point类型的anchor，表示锚点
+        . int borderType: int类型的borderType，用于推断图像外部像素的某种边界模式
+        """
         blur_mat = cv.blur(frame, (3, 3))
         # 高斯滤波
+        """
+        GaussianBlur（） 函数模型：
+        GaussianBlur( InputArray src, OutputArray dst, Size ksize,
+                                    double sigmaX, double sigmaY = 0,
+                                    int borderType = BORDER_DEFAULT );
+    
+        参数介绍：
+        . InputArray src：InputArray类型的src，可以是多通道的Mat类型的图像；但深度应CV_8U，CV_16U，CV_16S，CV_32F
+        . OutputArray dst：OutputArray类型的dst，即目标图像，函数的输出参数，需要和源图片有一样的尺寸和类型。
+        . Size ksize：高斯核大小
+        . double sigmaX, double sigmaY：X,Y方向上的高斯核标准偏差
+        . int borderType：int类型的borderType，用于推断图像外部像素的某种边界模式
+        """
         guass_mat = cv.GaussianBlur(frame, (3, 3), 1, 0)
         
         # 显示图片
@@ -36,6 +72,23 @@ def sobel_extraction():
     ret, frame = capture.read()
     
     # 利用sobel算子进行边缘提取
+    """
+    sobel（） 函数模型：
+        Sobel( InputArray src, OutputArray dst, int ddepth,
+                             int dx, int dy, int ksize = 3,
+                             double scale = 1, double delta = 0,
+                             int borderType = BORDER_DEFAULT );
+    
+        参数介绍：
+        . InputArray src：InputArray 类型的src，为输入图像，填Mat类型即可
+        . OutputArray dst：OutputArray类型的dst，即目标图像，函数的输出参数，需要和源图片有一样的尺寸和类型。
+        . int ddepth：int类型的ddepth，输出图像的深度
+        . int dx, int dy：X,Y方向上的差分阶数
+        . int ksize：sobel核的大小
+        . double scale：double类型的scale，计算导数值时可选的缩放因子，默认值是1
+        . double delta: double类型的delta，表示在结果存入目标图（第二个参数dst）之前可选的delta值，有默认值0
+        . int borderType：int类型的borderType，边界模式
+    """
     sobel_mat = cv.Sobel(frame, -1, 1, 0, 5)
     cv.imshow("sobel", sobel_mat)
     
