30f0e290d72b6828b58242620556e26cfcc9306f
lin ge <397367585@qq.com>
2020-09-16 15:20:30 +0800

    格式修改

---
+++ dip-class-demos.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+import numpy as np
+import function
+
+
+# 第八周练习1 轮毂中的圆孔定位
+function.rimblobanalysis()
+# 第八周练习2 芯片定位
+function.chipblobanalysis()
+# 第八周练习3 杯盖检测
+function.cap_blob()
+cv.waitKey(0)
