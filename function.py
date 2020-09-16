cb5fc79ab47816a169e310fc6f319bb2734a1852
lin ge <397367585@qq.com>
2020-09-16 08:50:57 +0800

    格式修改

--- function.py
+++ function.py
@@ -4,6 +4,8 @@ import numpy as np
 
 # 筛选2-n范围内的所有质数，并打印至状态栏
 def pickprime(maxval, ifprint):
+    print("pick out prime by trial division algorithm ")
+    # 筛选2-n范围内的质数
     num_sum = 0
     for i in range(2, maxval):
         if isprime(i):
@@ -15,16 +17,23 @@ def pickprime(maxval, ifprint):
                 print("")
     print("")
     print(f"the sum of prime is:{num_sum}")
+    
+    
 # 试除法检查质数
+# 假设有自然数x，如需要确定x是否为质数，只需确定x能否被2到√x范围内的奇数整除
 def isprime(number):
     sqrt_number = math.ceil(math.sqrt(number))+1
+    # 1及负数不为质数
     if number < 2:
         return False
+    # 2为最小质数
     elif number == 2:
         return True
+    # 偶数必不为质数
     elif number % 2 == 0:
         return False
     else:
+        # 确定能否被2到√x范围内的奇数整除
         for i in range(3, sqrt_number, 2):
             if number % i == 0:
                 return False        
@@ -38,10 +47,12 @@ def eratossieve(maxval,ifprint):
     flag[1] = False
     num_sum = 0
     for i in range(2,maxval+1):
+        # 如果第i号为质数
         if flag[i]:
             if ifprint:
                 print(f"{i} ", end ="")
             num_sum += 1
+            # 每20个换行
             if num_sum % 20 == 0:
                 print("")
             for j in range(i, maxval + 1, i):
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import cv2 as cv
 import function
 
+# 计算函数运行时间
 t1 = cv.getTickCount()
 n = 10000
 ifprint = True
+# 普通试除法
 function.pickprime(n, ifprint)
 t2 = cv.getTickCount()
 time_pickprime = (t2 - t1) / cv.getTickFrequency()
 print("time:%s ms" % (time_pickprime*1000))
+
+
 t3 = cv.getTickCount()
+# 埃拉托斯特尼筛选法
 function.eratossieve(n, ifprint)
 t4 = cv.getTickCount()
 time_eratossieve = (t4 - t3) / cv.getTickFrequency()
